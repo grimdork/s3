cdc4465e18d77ffbb7d30f9ced494c0da3c80b77
Urethramancer <github@grimdork.net>
2024-03-10 02:12:05 +0100

    Started adding check for existing destunation files.

---
+++ internal/cfg/strings.go
@@ -0,0 +1,6 @@
+package cfg
+
+const (
+	// GroupFlags is the name of the flags group in tool commands.
+	GroupFlags = "Flags"
+)
--- internal/cp/run.go
+++ internal/cp/run.go
@@ -1,15 +1,17 @@
 package cp
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/grimdork/climate/arg"
+	"github.com/grimdork/climate/paths"
 	"github.com/grimdork/s3/internal/cfg"
 	"github.com/grimdork/s3/internal/download"
 )
-
-const groupFlags = "Flags"
 
 // Run the command.
 func Run(opt *arg.Options) error {
@@ -21,8 +23,9 @@ func Run(opt *arg.Options) error {
 	// Flags
 	o := arg.New("s3 cp")
 	o.SetDefaultHelp(true)
-	o.SetFlag(groupFlags, "r", "recursive", "Copy recursively.")
-	o.SetFlag(groupFlags, "L", "list", "List of files to copy from the source. Ignores -r.")
+	o.SetFlag(cfg.GroupFlags, "r", "recursive", "Copy recursively.")
+	o.SetFlag(cfg.GroupFlags, "L", "list", "List of files to copy from the source. Ignores -r.")
+	o.SetFlag(cfg.GroupFlags, "f", "force", "Force overwrite.")
 	o.SetOption("Config", "p", "profile", "Profile to load.", profile, false, arg.VarString, nil)
 	o.SetOption("Config", "u", "url", "URL for the API.", cfg.Config.Profiles[profile], false, arg.VarString, nil)
 
@@ -42,13 +45,48 @@ func Run(opt *arg.Options) error {
 
 	srcurl := o.GetPosString("SOURCE")
 	desturl := o.GetPosString("DESTINATION")
-	// recursive := o.GetBool("r")
 	// list := o.GetString("List")
+	force := o.GetBool("f")
+
+	if o.GetBool("r") {
+		return arg.ErrRunCommand
+	}
 
 	src, size, err := download.OpenSource(srcurl)
 	if err != nil {
 		fmt.Printf("Error opening source: %s\n", err.Error())
 		os.Exit(2)
+	}
+
+	st := download.IdentifyURI(srcurl)
+	dt := download.IdentifyURI(desturl)
+
+	var s3c *s3.Client
+	if st == download.URIS3 || dt == download.URIS3 {
+		s3c, err = cfg.S3Client(profile, o.GetString("url"))
+		if err != nil {
+			fmt.Printf("Error loading credentials: %s\n", err.Error())
+			return arg.ErrRunCommand
+		}
+
+	}
+
+	if dt == download.URILocal && paths.FileExists(desturl) && !force {
+		fmt.Printf("Destination file exists. Use -f to overwrite.\n")
+		src.Close()
+		os.Exit(2)
+	}
+
+	if dt == download.URIS3 && !force {
+		goa := &s3.GetObjectAttributesInput{
+			Key: aws.String(desturl[4:]),
+		}
+		_, err = s3c.GetObjectAttributes(context.TODO(), goa)
+		if err == nil {
+			fmt.Printf("Destination file exists. Use -f to overwrite.\n")
+			src.Close()
+			os.Exit(2)
+		}
 	}
 
 	dest, err := download.OpenDestination(desturl)
@@ -64,12 +102,6 @@ func Run(opt *arg.Options) error {
 		fmt.Printf("Error copying: %s\n", err.Error())
 		os.Exit(2)
 	}
-
-	// s3c, err := cfg.S3Client(profile, o.GetString("url"))
-	// if err != nil {
-	// 	fmt.Printf("Error loading credentials: %s\n", err.Error())
-	// 	return arg.ErrRunCommand
-	// }
 
 	return arg.ErrRunCommand
 }
